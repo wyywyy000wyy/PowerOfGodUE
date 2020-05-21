69375df5da09a572f76fd0e17a91cfba65bd1cab
wangyiyi <599202537@qq.com>
2020-05-21 14:40:13 +0800

    new project

---
+++ Source/PowerOfGodUE/PowerOfGodUEGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PowerOfGodUEGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POWEROFGODUE_API APowerOfGodUEGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
